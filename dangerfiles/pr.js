ccd595c75b0ae04c9cd73a09dc0e0662ffd7a1aa
Marcos Ojeda <subliminal@gmail.com>
2017-09-12 12:11:25 -0700

    test updating dangerfiles

---
+++ dangerfiles/pr.js
@@ -0,0 +1,13 @@
+import {message, warn} from "danger";
+
+if (danger.github.pr.deletions > danger.github.pr.additions) {
+  message("🔥")
+}
+
+const packageChanged = danger.git.modified_files.includes('package.json');
+const lockfileChanged = danger.git.modified_files.includes('yarn.lock');
+if (packageChanged && !lockfileChanged) {
+  const message = 'Changes were made to package.json, but not to yarn.lock';
+  const idea = 'Perhaps you need to run `yarn install`?';
+  warn(`${message} - <i>${idea}</i>`);
+}
