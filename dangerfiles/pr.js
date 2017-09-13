6d2a2238f5dc2d34c9f41b40db10a181d990456a
Marcos Ojeda <subliminal@gmail.com>
2017-09-12 22:34:04 -0700

    simpler dangerfile

--- dangerfiles/pr.js
+++ dangerfiles/pr.js
@@ -7,11 +7,11 @@ if (danger.github.pr.deletions > danger.github.pr.additions) {
   message("🔥");
 }
 
-// keep lock files consistent
-const packageChanged = danger.git.modified_files.includes('package.json');
-const lockfileChanged = danger.git.modified_files.includes('yarn.lock');
-if (packageChanged && !lockfileChanged) {
-  const message = 'Changes were made to package.json, but not to yarn.lock';
-  const idea = 'Perhaps you need to run `yarn install`?';
-  warn(`${message} - <i>${idea}</i>`);
-}
+// // keep lock files consistent
+// const packageChanged = danger.git.modified_files.includes('package.json');
+// const lockfileChanged = danger.git.modified_files.includes('yarn.lock');
+// if (packageChanged && !lockfileChanged) {
+//   const message = 'Changes were made to package.json, but not to yarn.lock';
+//   const idea = 'Perhaps you need to run `yarn install`?';
+//   warn(`${message} - <i>${idea}</i>`);
+// }
